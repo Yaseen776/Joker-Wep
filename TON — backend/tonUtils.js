8ee5286d0999022dc5e91f4849bbc0c61bc9f8ad
Yaseen776 <133815328+Yaseen776@users.noreply.github.com>
2025-07-21 08:26:05 +0300

    Create tonUtils.js

---
+++ TON — backend/tonUtils.js
@@ -0,0 +1,5 @@
+// tonUtils.js
+module.exports.send = async function(fee, userTelegramId){
+  console.log(`Simulating TON transfer: ${fee} TON to user ${userTelegramId}`);
+  // هنا يمكنك دمج TonAPI أو TonConnect لإرسال الأموال فعليًا بمحفظة المستخدم
+}
